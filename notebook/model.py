d0671a120e1e941b19a0ac41d60cbec560341faf
Ali Shoeb <ashoeb81@gmail.com>
2018-09-23 05:24:59 -0700

    Uploading report.

--- notebook/model.py
+++ notebook/model.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 class QNetwork(nn.Module):
     """Actor (Policy) Model."""
@@ -11,21 +10,24 @@ class QNetwork(nn.Module):
         ======
             state_size (int): Dimension of each state
             action_size (int): Dimension of each action
+            num_units (list): Number of hidden-nodes in each fully-connected layer.
+              Note the model will have a number of fully-connected layers equal to
+              the size of num_units.
             seed (int): Random seed
-            fc1_units (int): Number of nodes in first hidden layer
-            fc2_units (int): Number of nodes in second hidden layer
         """
         super(QNetwork, self).__init__()
         self.seed = torch.manual_seed(seed)
+        # Add fully-connected layers.
         for idx, units in enumerate(num_units):
             if idx == 0:
                 modules = [nn.Linear(state_size, units)]
             else:
                 modules.append(nn.Linear(num_units[idx-1], units))
             modules.append(nn.ReLU())
+        # Add final layer with output equal to action space size.
         modules.append(nn.Linear(num_units[-1], action_size))
         self.model = nn.Sequential(*modules)
 
     def forward(self, state):
-        """Build a network that maps state -> action values."""
+        # Apply model to input state.
         return self.model(state)
